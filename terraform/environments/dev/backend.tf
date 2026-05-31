8b5bf0815cb5b68a337d8f66ba39342bcc7d35bd
sameer hadke <Sameerhadke51@gmail.com>
2026-05-31 08:40:30 +0530

    Fix formatting of backend configuration in backend.tf

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,8 +1,8 @@
 
 terraform {
-backend "s3" {
-bucket = "sameer-terraform-state2"
-key    = "terraform/dev.tfstate"
-region = "ap-south-1"
-}
+  backend "s3" {
+  bucket = "sameer-terraform-state2"
+  key    = "terraform/dev.tfstate"
+  region = "ap-south-1"
+ }
 }
